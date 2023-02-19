515c086f1da9e7a340a492d91a2e9a222fc9368f
jnewbre <github.cco7i@simplelogin.com>
2023-02-19 18:01:46 -0500

    Created variable.tf

---
+++ variable.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "key_name" {
+  description = "EC2 key name"
+  type        = string
+  default     = "sde-key"
+}
+
+variable "instance_type" {
+  description = "Instance type for EMR and EC2"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "repo_url" {
+  description = "Repository url to clone into production machine"
+  type        = string
+  default     = "https://github.com/jnewbre/YelpMapDashboard.git"
+}
