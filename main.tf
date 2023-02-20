7990fd40ab9c1fc3411026f3c40d9a81eea3957a
jnewbre <github.cco7i@simplelogin.com>
2023-02-19 21:58:54 -0500

    Formatting

--- main.tf
+++ main.tf
@@ -98,23 +98,22 @@ resource "aws_instance" "map_ec2" {
 
         echo "-------------------------END SETUP---------------------------"
     EOF
-
 }
 
 resource "aws_budgets_budget" "ec2" {
-  name              = "budget-ec2-monthly"
-  budget_type       = "COST"
-  limit_amount      = "5"
-  limit_unit        = "USD"
-  time_period_end   = "2087-06-15_00:00"
-  time_period_start = "2023-02-19_00:00"
-  time_unit         = "MONTHLY"
+    name              = "budget-ec2-monthly"
+    budget_type       = "COST"
+    limit_amount      = "5"
+    limit_unit        = "USD"
+    time_period_end   = "2087-06-15_00:00"
+    time_period_start = "2023-02-19_00:00"
+    time_unit         = "MONTHLY"
 
-  notification {
-    comparison_operator        = "GREATER_THAN"
-    threshold                  = 100
-    threshold_type             = "PERCENTAGE"
-    notification_type          = "FORECASTED"
-    subscriber_email_addresses = [var.alert_email_id]
-  }
+    notification {
+        comparison_operator        = "GREATER_THAN"
+        threshold                  = 100
+        threshold_type             = "PERCENTAGE"
+        notification_type          = "FORECASTED"
+        subscriber_email_addresses = [var.alert_email_id]
+    }
 }
